4fe6925284e9c965c2d00a8f53a7a8925a2d843c
tuananhudu <buiduytuananhudu@gmail.com>
2025-06-22 10:44:03 +0700

    code file seed_data in FAISS

---
+++ src/seed_data.py
@@ -0,0 +1,101 @@
+import os 
+import json 
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings , OllamaEmbeddings
+from langchain.docstore.document import Document
+from crawl import craw_web
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_data_from_local(filename : str , directory : str) -> tuple: 
+    file_path = os.path.join(directory , filename)
+    
+    with open(file_path , 'r') as file : 
+        data = json.load(file)
+    
+    print(f"Data loaded from {file_path}")
+    
+    return data , filename.rsplit('.' , 1)[0].replace('_' , ' ')
+
+def seed_faiss(collection_name : str  , filename : str , directory : str , use_ollama : bool = False , save_path: str = "./vectorstores") -> FAISS : 
+    if use_ollama : 
+        embeddings = OllamaEmbeddings(model="llama2")
+    else : 
+        embeddings = HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2")
+    
+    # Đọc dữ liệu từ file_local : 
+    local_data , doc_name = load_data_from_local(filename , directory)
+    
+    # Chuyên dữ liệu thành documents :
+    documents = [
+        Document(
+            page_content=doc.get('page_content') or '',
+            metadata={
+                'source': doc['metadata'].get('source') or '',
+                'content_type': doc['metadata'].get('content_type') or 'text/plain',
+                'title': doc['metadata'].get('title') or '',
+                'description': doc['metadata'].get('description') or '',
+                'language': doc['metadata'].get('language') or 'en',
+                'doc_name': doc_name,
+                'start_index': doc['metadata'].get('start_index') or 0
+            }
+        )
+        for doc in local_data
+    ]
+    vectorstore = FAISS.from_documents(documents , embedding= embeddings)
+    
+    os.makedirs(save_path, exist_ok=True)
+    vectorstore.save_local(os.path.join(save_path, collection_name))
+
+    print(f"FAISS vectorstore saved to {os.path.join(save_path, collection_name)}")
+    return vectorstore
+
+def seed_faiss_live(URL : str , collection_name : str , doc_name : str , use_ollama : bool , save_path: str = "./vectorstores" ) -> FAISS :   
+    if use_ollama : 
+        embeddings = OllamaEmbeddings(model="llama2")
+    else : 
+        embeddings = HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2")
+    
+    documens = craw_web(URL)
+    
+    # Cập nhật metada cho mỗi documents với giá trị mặc định 
+    for doc in documens : 
+        metadata = {
+            'source' : doc.metadata.get('source') or '',
+            'content_type' : doc.metadata.get('content_type') or '',
+            'title': doc.metadata.get('title') or '',
+            'description': doc.metadata.get('description') or '',
+            'language': doc.metadata.get('language') or 'en',
+            'doc_name': doc_name,
+            'start_index': doc.metadata.get('start_index') or 0
+        }
+        doc.metadata = metadata
+    
+    vectorstore = FAISS.from_documents(documens , embeddings)
+    os.makedirs(save_path , exist_ok= True)
+    vectorstore.save_local(os.path.join(save_path , collection_name))
+    print(f"FAISS vectorstore saved to {os.path.join(save_path, collection_name)}")
+    return vectorstore
+    
+    
+def main():
+#     # Test seed_faiss với dũ liệu local
+#     seed_faiss(
+#     collection_name= 'data_test'
+#     filename='stack.json',
+#     directory='data',
+#     use_ollama=False
+# )       
+    # Test seed_faiss_live với dữ liệu trực tiếp 
+    seed_faiss_live(
+        URL='https://www.stack-ai.com/docs',
+        collection_name= "data_test_live",
+        doc_name='stack-ai',
+        use_ollama= False 
+    )
+    
+
+# Chạy main() nếu file được thực thi trực tiếp
+if __name__ == "__main__":
+    main()
